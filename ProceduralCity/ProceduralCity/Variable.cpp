0b32af8a36f22d211f9d62eec44059411b1f1ec6
Rob Carlan <robert.carlan@keble.ox.ac.uk>
2016-01-02 19:10:32 +0000

    Added rendering and updated UI

---
+++ ProceduralCity/ProceduralCity/Variable.cpp
@@ -0,0 +1,8 @@
+#include "Variable.h"
+
+Variable::Variable() {
+}
+
+
+Variable::~Variable() {
+}
---
+++ ProceduralCity/ProceduralCity/Variable.h
@@ -0,0 +1,27 @@
+#pragma once
+
+//Represents the basic variable
+class Variable {
+public:
+	Variable();
+	~Variable();
+};
+
+struct ruleAttr {
+
+};
+
+struct roadAttr {
+
+};
+
+class RoadVar {
+public:
+	int delay;
+
+};
+
+class InsertionVar {
+
+};
+
